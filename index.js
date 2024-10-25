84f2660bcb80952deda6848fe2fd5d407448a832
bismuth <70106649+bismuthnickel@users.noreply.github.com>
2024-10-25 08:24:06 -0400

    fail

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 const fs = require("fs");
 const http = require("http");
 
-const config = JSON.parse(fs.readFileSync("./config.json"));
-
-var basePath = config.templatesFolder;
 var path = require('path');
 
-http.createServer(function(req, res) {
-    var stream = fs.createReadStream(path.join(basePath, req.url));
-    stream.on('error', function() {
-        if (fs.existsSync(path.join(basePath, "404.html"))) {
-            fs.createReadStream(path.join(basePath, "404.html")).pipe(res);
-        } else {
-            res.write("<h1>404</h1>default 404 error");
-            res.end();
-        }
-    });
-    stream.pipe(res);
-}).listen((config.port), () => {
-    console.log(config.message);
-    console.log("running at http://localhost:"+config.port);
-});
+const komikos = (port,templatePath,callback) => {
+    http.createServer(function(req, res) {
+        var stream = fs.createReadStream(path.join(templatePath, req.url));
+        stream.on('error', function() {
+            if (fs.existsSync(path.join(templatePath, "404.html"))) {
+                fs.createReadStream(path.join(templatePath, "404.html")).pipe(res);
+            } else {
+                res.write("<h1>404</h1>default 404 error");
+                res.end();
+            }
+        });
+        stream.pipe(res);
+    }).listen((port), callback);
+}
+
+module.exports = komikos
